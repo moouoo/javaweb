85388030da5881d5c4e4b7b5ca07836298a9bae4
moouoo <0109963@naver.com>
2023-05-03 11:00:40 +0900

    20230503

---
+++ src/main/java/guest/GuestListCommand.java
@@ -0,0 +1,58 @@
+package guest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestListCommand implements GuestInterface {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GuestDAO dao = new GuestDAO();
+		
+		// 1. 현재 페이지번호를 구한다.
+		int pag = request.getParameter("pag")==null ? 1 : Integer.parseInt(request.getParameter("pag"));
+		
+		// 2. 한페이지의 분량을 결정한다.
+		int pageSize = request.getParameter("pageSize")==null ? 5 : Integer.parseInt(request.getParameter("pageSize"));
+		
+		// 3. 총 레코드 건수를 구한다.
+		int totRecCnt = dao.getTotRecCnt();
+		
+		// 4. 총 페이지 건수를 구한다.
+		int totPage = (totRecCnt % pageSize)==0 ? (totRecCnt / pageSize) : (totRecCnt / pageSize) + 1 ;
+		
+		// 5. 현재페이지의 '시작 인덱스번호'를 구한다.
+		int startIndexNo = (pag - 1) * pageSize;
+		
+		// 6. 현재 화면에 표시할 시작번호를 구한다.
+		int curScrStartNo = totRecCnt - startIndexNo;
+		
+		
+		// 블록페이징처리.... 블록의 시작번호를 0부터 처리했다.
+		// 1. 블록의 크기를 결정한다.(여기선 3으로 결정후 처리)
+		int blockSize = 3;
+		
+		// 2. 현재페이지가 속한 블록 번호를 구한다.(예:1페이지는 0블록, 2페이지도 0블록, 4~6페이지는 1블록)
+		int curBlock = (pag - 1) / blockSize;
+		
+		// 3. 마지막 블록을 구한다.
+		int lastBlock = (totPage - 1) / blockSize;
+		
+		// 지정된 페이지의 자료를 요청한 한페이지 분량만큼 가져온다.
+		ArrayList<GuestVO> vos = dao.getGuestList(startIndexNo, pageSize);
+		
+		request.setAttribute("vos", vos);
+		request.setAttribute("pag", pag);
+		request.setAttribute("totPage", totPage);
+		request.setAttribute("curScrStartNo", curScrStartNo);
+		request.setAttribute("pageSize", pageSize);
+		request.setAttribute("blockSize", blockSize);
+		request.setAttribute("curBlock", curBlock);
+		request.setAttribute("lastBlock", lastBlock);
+	}
+
+}
